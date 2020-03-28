d38236f91c17a4873cc94a30352669f573978551
linghuchong <linghuchong777888@126.com>
2020-03-28 15:46:02 +0800

    dugu

--- src/main/java/com/atguigu/jianfa/Dugu9jian.java
+++ src/main/java/com/atguigu/jianfa/Dugu9jian.java
@@ -2,6 +2,9 @@ package com.atguigu.jianfa;
 
 public class Dugu9jian {
     public static void main(String[] args) {
-        System.out.println("99999999jian ");
+        System.out.println("dugu ");
+        System.out.println("dugu ");
+        System.out.println("dugu ");
+        System.out.println("dugu ");
     }
 }
