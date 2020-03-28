4f401dbcac229c1dee2b565a0280e764440f8c25
yuebuqun <yuebuqun777888@126.com>
2020-03-28 15:40:04 +0800

    hua shan jianfa

--- src/main/java/com/atguigu/jianfa/Dugu9jian.java
+++ src/main/java/com/atguigu/jianfa/Dugu9jian.java
@@ -3,5 +3,8 @@ package com.atguigu.jianfa;
 public class Dugu9jian {
     public static void main(String[] args) {
         System.out.println("99999999jian ");
+        System.out.println("99999999jian ");
+        System.out.println("99999999jian ");
+        System.out.println("99999999jian ");
     }
 }
